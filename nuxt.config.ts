43e9caa912a309a3029df7ce76daf3fd86eb664d
Facundo David Gonzalez <fac.gonzalez@outlook.com>
2023-08-18 01:29:11 -0300

    add metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      title: "Framework Battle Royale Nuxt",
+      meta: [
+        { name: "description", content: "Framework Battle Royale Nuxt" },
+        {
+          name: "keywords",
+          content: "html, css, javascript, frontend, vue, nuxt",
+        },
+      ],
+    },
+  },
 });
